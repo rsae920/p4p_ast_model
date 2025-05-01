2ce1e35ddfa895336be68bcc6f162677c28e097d
rsae920 <100651778+rsae920@users.noreply.github.com>
2025-05-02 11:06:33 +1200

    latest commit

--- model.py
+++
@@ -1,5 +0,0 @@
-from transformers import ASTForAudioClassification
-
-def create_model(num_labels):
-    model = ASTForAudioClassification.from_pretrained("MIT/ast-finetuned-audioset-10-10-0.4593", num_labels=num_labels)
-    return model
